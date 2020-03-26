01b96c521f2c803e5976b2daceeab1730900edb4
ffmiyo <leffmiyo@gmail.com>
2020-03-26 16:05:00 +0800

    Initial templating

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"text/template"
+
 	hn "github.com/montanaflynn/hn/hnclient"
 )
 
@@ -16,16 +19,53 @@ type Node struct {
 	Nodes []*Node
 }
 
+var t = template.Must(template.New("").Parse(`
+{{define "mainStory"}}
+<div id="post">                                              
+  {{.Text}} by {{.By}} 
+  {{template "comments" .Nodes}}
+</div>
+{{end}}
+
+{{define "comments"}}
+   {{if .}}
+      <ul>
+      {{range . }}                                  
+         <li class="post">                                         
+           <div class="postHead">                                  
+             <div class="postTitle"><b>{{.By}}</b></div>   
+           </div>
+           <div class="postBody">{{.Text}}</div>
+           {{template "comments" .Nodes}}
+         </li>
+      {{end}}
+      </ul>
+   {{end}}
+{{end}}
+`))
+
 func main() {
 	client := hn.NewClient()
 	astory, _ := client.GetItem(22675078)
 	rootStory := &Node{
+		Id:    astory.Id,
 		Kids:  astory.Kids,
+		Text:  astory.Title,
+		Url:   astory.Url,
+		By:    astory.By,
 		Nodes: make([]*Node, len(astory.Kids)),
 	}
 	fillNode(rootStory, client)
 
-	fmt.Println(rootStory.Nodes[0].Nodes[1].Text)
+	file, err := os.Create(rootStory.Url + ".html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	if err := t.ExecuteTemplate(file, "mainStory", rootStory); err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func fillNode(node *Node, client *hn.Client) {
@@ -35,10 +75,8 @@ func fillNode(node *Node, client *hn.Client) {
 		node.Nodes[i] = ItemToNode(item)
 		if len(item.Kids) > 0 {
 			node.Nodes[i].Nodes = make([]*Node, len(item.Kids))
-			fmt.Println(node.Nodes[i].Kids)
 			fillNode(node.Nodes[i], client)
 		}
-		fmt.Println(node.Nodes[i].Nodes)
 	}
 }
 
